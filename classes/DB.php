058001654bca015c7f4d18f6c3c2380b724f362a
Kokosalah45 <kokp.amged@gmail.com>
2021-05-21 11:10:38 +0200

    some db work

--- classes/DB.php
+++ classes/DB.php
@@ -12,21 +12,50 @@ class DB{
     {
         try{
             $this->_pdo= new PDO( "mysql:host=".Config::get('mysql/host').";dbname=".Config::get('mysql/db') , Config::get('mysql/username'), Config::get('mysql/password') );
-            echo 'Connected !';
+
         }catch (PDOException $e){
             die($e->getMessage());
-
         }
     }
     public static  function  getInstance (){
         if (!isset(self::$instance)){
             self::$instance = new DB();
+        }
+
+        return self::$instance;
+    }
+    public function query ($sql,$params = array()){
+        $this->_error=false;
+        if ($this->_query = $this->_pdo->prepare($sql) ){
+            $x=1;
+            if(count($params)){
+                foreach($params as $param){
+                    $this->_query->bindValue($x,$param);
+                    $x++;
+                }
+            }
 
         }
-        return self::$instance;
+        if($this->_query->execute()){
+            $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+            $this->_count = $this->_query->rowCount();
+        }else{
+            $this->_error=true;
+        }
 
-
+        return $this;
     }
+    public function error(){
+        return 'de7k';
+    }
+
+    //add user
+    //view user
+    //delete user
+    //
+
+
+
 
 
 }
--- lndex.php
+++ lndex.php
@@ -1,4 +1,13 @@
 <?php
 require_once 'core/init.php';
+ $DB0 = DB::getInstance()->query('SELECT USERNAME FROM USERS ');
+ $DB1 = DB::getInstance()->query('SELECT USERNAME FROM USERS ');
+ $DB2 = DB::getInstance()->query('SELECT USERNAME FROM USERS ');
+echo  $DB0->error();
+echo  $DB1->error();
+echo  $DB2->error();
 
- DB::getInstance();
+
+
+
+
